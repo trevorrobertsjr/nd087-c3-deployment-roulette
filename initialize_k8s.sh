3a32b2d5f8310709407dc6dcd102ca4aea62b99e
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-02-13 08:13:29 -0500

    deployment troubleshooting and canary complete

--- initialize_k8s.sh
+++ initialize_k8s.sh
@@ -6,3 +6,6 @@ kubectl apply -f starter/apps/hello-world
 kubectl apply -f starter/apps/canary/index_v1_html.yml
 kubectl apply -f starter/apps/canary/canary-v1.yml
 kubectl apply -f starter/apps/blue-green
+
+kubectl apply -f starter/apps/canary/index_v2_html.yml
+kubectl apply -f starter/apps/canary/canary-v2.yml
---
+++ solutions/canary.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DEPLOY_INCREMENTS=1
+
+function canary_deploy {
+  NUM_OF_V1_PODS=$(kubectl get pods -n udacity | grep -c canary-v1)
+  echo "V1 PODS: $NUM_OF_V1_PODS"
+  NUM_OF_V2_PODS=$(kubectl get pods -n udacity | grep -c canary-v2)
+  echo "V2 PODS: $NUM_OF_V2_PODS"
+
+  kubectl scale deployment canary-v2 --replicas=$((NUM_OF_V2_PODS + $DEPLOY_INCREMENTS))
+  kubectl scale deployment canary-v1 --replicas=$((NUM_OF_V1_PODS - $DEPLOY_INCREMENTS))
+  # Check deployment rollout status every 1 second until complete.
+  ATTEMPTS=0
+  ROLLOUT_STATUS_CMD="kubectl rollout status deployment/canary-v2 -n udacity"
+  until $ROLLOUT_STATUS_CMD || [ $ATTEMPTS -eq 60 ]; do
+    $ROLLOUT_STATUS_CMD
+    ATTEMPTS=$((attempts + 1))
+    sleep 1
+  done
+  echo "Canary deployment of $DEPLOY_INCREMENTS replicas successful!"
+}
+
+# Initial canary-v2 deployment
+kubectl apply -f ../starter/apps/canary/index_v2_html.yml
+kubectl apply -f ../starter/apps/canary/canary-v2.yml
+
+sleep 1
+# Begin canary deployment
+while [ $(kubectl get pods -n udacity | grep -c canary-v1) -ne $(kubectl get pods -n udacity | grep -c canary-v2) ]
+do
+  canary_deploy
+done
+# Verify at 50% deployment for each version
+NUM_OF_V1_PODS=$(kubectl get pods -n udacity | grep -c canary-v1)
+echo "V1 PODS: $NUM_OF_V1_PODS"
+NUM_OF_V2_PODS=$(kubectl get pods -n udacity | grep -c canary-v2)
+echo "V2 PODS: $NUM_OF_V2_PODS"
+echo "Canary deployment of v2 successful"
