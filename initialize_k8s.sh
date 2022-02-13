1ab3e592a8be5cd609492558c1a8fd3eb877039d
Trevor Roberts Jr <Trevor.Roberts.Jr@gmail.com>
2022-02-13 12:11:02 -0500

    update initialization to deploy canary service

--- initialize_k8s.sh
+++ initialize_k8s.sh
@@ -5,7 +5,5 @@ set -e
 kubectl apply -f starter/apps/hello-world
 kubectl apply -f starter/apps/canary/index_v1_html.yml
 kubectl apply -f starter/apps/canary/canary-v1.yml
+kubectl apply -f starter/apps/canary/canary-svc.yml
 kubectl apply -f starter/apps/blue-green
-
-kubectl apply -f starter/apps/canary/index_v2_html.yml
-kubectl apply -f starter/apps/canary/canary-v2.yml
